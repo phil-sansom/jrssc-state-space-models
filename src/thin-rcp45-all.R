edd19fb88bda7910201767ac89c97682e2966322
Phil Sansom <p.g.sansom@exeter.ac.uk>
2021-01-20 15:02:04 +0000

    Revert "Initial commit"
    This reverts commit 2c48dcbddb82e6cbc13847c39d4bcab77df0b92c.

--- src/thin-rcp45-all.R
+++
@@ -1,6 +0,0 @@
-## Load source
-source("./src/load-wishart.R")
-
-samples = load.samples("samples-rcp45-all", "output", 10, 200)
-
-save(samples, file = "./output/samples-rcp45-all-10k.RData")
--- src/thin-rcp45-flat.R
+++
@@ -1,6 +0,0 @@
-## Load source
-source("./src/load-wishart.R")
-
-samples = load.samples("samples-rcp45-flat", "output", 10, 200)
-
-save(samples, file = "./output/samples-rcp45-flat-10k.RData")
